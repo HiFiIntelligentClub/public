c032aaeab95ff0326fd2194273e396801241935d
Andrey Chekmarev <assminog@gmail.com>
2021-03-17 13:48:23 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,38 +1,118 @@
 #!/usr/bin/php
 <?php
+/*© A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru
+© Andrey Chekmaryov 2021
+
+Email:    assminog@gmail.com
+Email:    tubmulur@yandex.ru
+Phone:    +7(911)787-44-57
+Whatsapp: +7(911)787-44-57
+Telegram: https://t.me/HficSamin
+VK:       https://vk.com/Hfic.Samin
+VK:       https://vk.com/HiFiIntelligentClub
+Facebook: https://facebook.com/Hfic.Samin
+Facebook: https://facebook.com/HiFiIntelligentClub
+Site[Ru] Public browsing international:  http://HiFiIntelligentClub.Ru
+Site[En] Public browsing international:  http://HiFiIntelligentClub.COM
+Site[En] Private browsing international: http://ryklzxobxv4s32omimbu7d7t3cdw6dplvsz36zsqqu7ad2foo5m3tmad.onion
+|E    |D     |R      |O      |
+|Event|Design|Reality|Objects|
+ //////                     /////  / 
+//        /\                  // /   
+//      <  **>               /// /  
+ //////   jl                ///// /    
+./././././././*/ //КИМ in latin is KIIM Иcкуственный Интеллект Кирилл и Мефодий (). 
+			/*_\\KIIM//__Start_____*/ 
+			/*__\\  //___Checkpiont*/ 
+			/*___\\//____Finish___ */ 
+			/*____\/ECTOR <-->_____*/ 
+	//if finish than compete if the result EXIST! Filosophy
+
+//My lasts impulse of power 
+// ---------------
+// | $rRadio     |<--.
+// |/////////////|   ||                      Request processing2EDRO: 	Абхазия serve;
+// ---------------   ||                      Store images:		Saint-Petersbourg serve;
+//       |  .--FALSE-'                       Store personal messages:  	Moscow serve;
+//       V /         || GET Listener         Store music: 		Chechnia serve;
+// ----rRadio.Step-- ^--------------------
+//       |           ||
+//       | .--FALSE--'
+//       V/
+// ---rRadio.Step
+*/
 set_time_limit(0);
-while(фКИМ('CheckMaLoad', $мЗагрузка))
-  {
-  require('/home/EDRO.SetOfTools/System/0.Loader/0.loader.php');
-  require('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
-  require('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
-  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_CitizenCountry1_Country2_.php');
-  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_УКРФ.php');
-  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViloationLaw_ResidenceCountry.php');
-  require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViolation_ResourceRules.php');
+$мЗагрузка	=array();
+$мЗагрузка[]	=
+array(
+	'дВремяСтарт'		=> round(microtime(true), 4)
+	'дВремяШаг'		=> 
+	'дДельтаВремяСтарт'	=> round(microtime(true), 4),
+	'дДельтаЧётВремяШаг'	=> 
+	'дЦелЗад'		=> 0, //Десятичная Дробь, Целостность задачи, выполнено, когда равно 1.
+	);
+function мКИМ($ч0Шаг, $мЗагрузка)
+	{
+	//$мЗагрузка['дВремяСтарт']	=
+	$мЗагрузка[$ч0Шаг]['дВремяШаг']		= 
+	$мЗагрузка[$ч0Шаг]['дДельтаВремяСтарт']	=
+	$мЗагрузка[$ч0Шаг]['дДельтаЧётВремяШаг']=
+	$мЗагрузка[$ч0Шаг]['ч0Шаг']		= $ч0Шаг++,
+	$мЗагрузка[$ч0Шаг]['дЦелЗад']		= //Десятичная Дробь, Целостность задачи, выполнено, когда равно 1.
 
-//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SendMail/');
-//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Complains');
-//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResidenceCountryLaw');
-//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResurceRules');
-//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/Rights_ResidentCounttry1_residentCountry2');
-  require('/home/EDRO.SetOfTools/System/2.Functions/0.key.php');
-  require('/home/EDRO.SetOfTools/System/2.Functions/1.FunctionsSetup.php');
-  require('/home/EDRO.SetOfTools/System/2.Functions/2.Platforms.php');
-  require('/home/EDRO.SetOfTools/System/2.Functions/3.Functions.php');
-  require('/home/EDRO.SetOfTools/System/2.Functions/4.Dyn.php');
-  require('/home/EDRO.SetOfTools/System/2.Functions/5.StringFunctions.php');
-  require('/home/EDRO.SetOfTools/System/2.Functions/5.StringFunctions.php');
-  require('/home/EDRO.SetOfTools/System/3.VectorKIIM/0.KIIM.php');
-  require('/home/EDRO.SetOfTools/System/3.VectorKIIM/1.objKIIM.activation.php');
-  require('/home/EDRO.SetOfTools/System/3.VectorKIIM_Helper/1.objKIIM.activation.php');
-  require('/home/EDRO.SetOfTools/System/4.RAM/0.EDRO_Objects.php');
-  require('/home/EDRO.SetOfTools/System/5.Styles/0.CSS.Styles.php');
-  require('/home/EDRO.SetOfTools/System/6.HTML_Interfaces/0.HTML_HeadInterface.php');
-  require('/home/EDRO.SetOfTools/System/7.Templates/0.strKIIM.Template.php');
+	array(
+		'дВремяСтарт'		=> 
+		'дВремя'		=> round(microtime(true), 4),
+		'ч0Шаг'			=> $ч0Шаг,
+		'дДельтаЧёт'		=> round(microtime(true), 4),
+		'дЦелЗад'		=> round(microtime(true), 4), //Десятичная Дробь, Целостность задачи, выполнено, когда равно 1.
+		);
+	}
 
-  require_once('/home/EDRO.SetOfTools/System/3.Buffer/1.EDRO_Buffering.php');
-  }
+
+
+$мЗагрузка	=
+array(
+	'дВремяСтарт'		=> round(microtime(true), 4)
+	'дВремя'		=> round(microtime(true), 4),
+	'ч0Шаг'			=> 0,
+	'дДельтаЧёт'		=> 0.00,
+	'дЦелЗад'		=> 0, //Десятичная Дробь, Целостность задачи, выполнено, когда равно 1.
+	);
+$мЗагрузка	=мКИМ($мЗагрузка);
+
+while($мЗагрузка=фКИМ($мЗагрузка))
+	{
+	
+	require('/home/EDRO.SetOfTools/System/0.Loader/0.loader.php');
+	require('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
+	require('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
+	require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_CitizenCountry1_Country2_.php');
+	require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportComplain_Violation_УКРФ.php');
+	require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViloationLaw_ResidenceCountry.php');
+	require('/home/EDRO.SetOfTools/System/1.Reporter/2.ReportViolation_ResourceRules.php');
+
+	//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SendMail/');
+	//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Complains');
+	//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResidenceCountryLaw');
+	//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/ResurceRules');
+	//require('/home/EDRO.SetOfTools/System/1.Оповещение/ОтправкаПисьмаАдминистраторуСистемы/ПочтовыйРобот/SuspectKeys/Violation/Rights_ResidentCounttry1_residentCountry2');
+	require('/home/EDRO.SetOfTools/System/2.Functions/0.key.php');
+	require('/home/EDRO.SetOfTools/System/2.Functions/1.FunctionsSetup.php');
+	require('/home/EDRO.SetOfTools/System/2.Functions/2.Platforms.php');
+	require('/home/EDRO.SetOfTools/System/2.Functions/3.Functions.php');
+	require('/home/EDRO.SetOfTools/System/2.Functions/4.Dyn.php');
+	require('/home/EDRO.SetOfTools/System/2.Functions/5.StringFunctions.php');
+	require('/home/EDRO.SetOfTools/System/2.Functions/5.StringFunctions.php');
+	require('/home/EDRO.SetOfTools/System/3.VectorKIIM/0.KIIM.php');
+	require('/home/EDRO.SetOfTools/System/3.VectorKIIM/1.objKIIM.activation.php');
+	require('/home/EDRO.SetOfTools/System/3.VectorKIIM_Helper/1.objKIIM.activation.php');
+	require('/home/EDRO.SetOfTools/System/4.RAM/0.EDRO_Objects.php');
+	require('/home/EDRO.SetOfTools/System/5.Styles/0.CSS.Styles.php');
+	require('/home/EDRO.SetOfTools/System/6.HTML_Interfaces/0.HTML_HeadInterface.php');
+	require('/home/EDRO.SetOfTools/System/7.Templates/0.strKIIM.Template.php');
+	require_once('/home/EDRO.SetOfTools/System/3.Buffer/1.EDRO_Buffering.php');
+	}
 
 
 $objEDRO=new EDRO();
@@ -43,9 +123,6 @@ echo $objEDRO->strHTML();
 
 
 /*
-
-
-
 */
 
 ?>
